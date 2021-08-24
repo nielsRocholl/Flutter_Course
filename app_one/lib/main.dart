d788915be0753da6339ea9a067cfe4e874e0e158
nielsRocholl <nielsrocholl@gmail.com>
2021-08-24 10:52:13 +0200

    added questions.dart

--- app_one/lib/main.dart
+++ app_one/lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main(List<String> args) {
 //   runApp(MyApp());
 // }
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite colour?',
+      'What\'s your favotite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first app"),
         ),
-        body: Text("some text"),
+        body: Column(
+          children: [
+            Question(questions.elementAt(_questionIndex)),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(
+                onPressed: () => print('answer 2 chosen'),
+                child: Text('Answer !r')),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
